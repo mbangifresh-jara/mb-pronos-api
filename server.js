81ef83438ef3d085e00f281a44abc72c7b281533
mbangifresh-jara <mbangifresh@gmail.com>
2026-05-06 09:15:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.get('/matches', async (req, res) => {
   try {
     const now = new Date();
-    const in14 = new Date(Date.now() + 14*24*3600*1000);
+    const in9 = new Date(Date.now() + 9*24*3600*1000);
     const fmt = d => d.toISOString().split('T')[0];
     const r = await fetch(
-      `https://api.football-data.org/v4/matches?status=SCHEDULED&dateFrom=${fmt(now)}&dateTo=${fmt(in14)}`,
+      `https://api.football-data.org/v4/matches?status=SCHEDULED&dateFrom=${fmt(now)}&dateTo=${fmt(in9)}`,
       { headers: { 'X-Auth-Token': '8dfb32a730f5488cad5fb3926cbdf2cd' } }
     );
     const d = await r.json();
